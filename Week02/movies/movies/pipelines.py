a5753e35087def0bf8e3652ad938c80dec73bcc8
spades_k_ubuntu <13793505895@139.com>
2020-10-16 19:24:42 +0800

    delete comments

--- Week02/movies/movies/pipelines.py
+++ Week02/movies/movies/pipelines.py
@@ -14,14 +14,6 @@ class MoviesPipeline:
         movie_type = item['movie_type']
         movie_date = item['movie_date']
 
-        # 原有pandas方式保存数据
-        # output_list = [title, movie_type, movie_date]
-
-        # movie1 = pd.DataFrame(data=output_list)
-        # mode = 'a+' 进行追加写入
-        # movie1.to_csv('./movies.csv', encoding='gbk',mode='a+', index=False, header=False)
-        # movie1.to_csv('./movies.csv', encoding='utf-8',mode='a+', index=False, header=False)
-
 
         # pymysql操作mysql方式保存数据
         connection = pymysql.connect( **mysql_info,
